a3155e55ec20d66e86f102bd463fd14e1b84ee6a
zanhao <zanhao1234@126.com>
2018-04-21 16:25:20 +0800

    上市公司社会责任报告相关数据下载

--- stock_project/scrapy/spider/CEScrapy.py
+++ stock_project/scrapy/spider/CEScrapy.py
@@ -7,6 +7,7 @@ import requests
 import time
 import xlrd
 import xlwt
+import demjson
 from pyquery import PyQuery as pq
 from scrapy.scrapy import scrapy
 
@@ -32,5 +33,46 @@ class CEScrapy(scrapy):
         self.timeout = 10
 
     def search(self):
-        self.session.get('')
-        return None
+        com_list = []
+        self.session.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+        self.session.headers['Referer'] = 'http://stockdata.stock.hexun.com/zrbg/Plate.aspx?date=2016-12-31'
+        self.session.headers['Host'] = 'stockdata.stock.hexun.com'
+        for x in xrange(1, 180):
+            response = self.session.get('http://stockdata.stock.hexun.com/zrbg/data/zrbList.aspx?date=2016-12-31&count=20&pname=20&titType=null&page=%s&callback=hxbase_json11524298566134'%str(x), timeout=self.timeout, verify=False)
+            res = demjson.decode(response.text.replace('hxbase_json1(', '')[0:-1].encode('utf-8'))
+            for tr in res['list']:
+                logging.info(json.dumps(tr))
+                com_list.append(self.parse(tr))
+        self.write_xls(com_list)
+
+    def write_xls(self, com_list):
+        excel = xlwt.Workbook()
+        sheet01 = excel.add_sheet('2016')
+        for index, tr in enumerate(com_list):
+            sheet01.write(index, 0, tr['company'])
+            sheet01.write(index, 1, tr['stockNumber'])
+            sheet01.write(index, 2, tr['industryrate'])
+            sheet01.write(index, 3, tr['Pricelimit'])
+            sheet01.write(index, 4, tr['lootingchips'])
+            sheet01.write(index, 5, tr['Scramble'])
+            sheet01.write(index, 6, tr['rscramble'])
+            sheet01.write(index, 7, tr['Strongstock'])
+        excel.save('2016.xls')
+
+    def parse(self, response):
+        company_detail = {}
+        company_detail['company'] = response['industry']
+        company_detail['stockNumber'] = response['stockNumber']
+        company_detail['industryrate'] = response['industryrate']
+        company_detail['Pricelimit'] = response['Pricelimit']
+        company_detail['lootingchips'] = response['lootingchips']
+        company_detail['Scramble'] = response['Scramble']
+        company_detail['rscramble'] = response['rscramble']
+        company_detail['Strongstock'] = response['Strongstock']
+        return company_detail
+
+
+if __name__ == '__main__':
+    config_log()
+    res = CEScrapy()
+    res.search()
